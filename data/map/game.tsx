86fd068d66acac6f3da3ae5499dc6a59f85b4fb1
Gumichan01 <luxon.jean.pierre@gmail.com>
2018-06-08 19:27:30 +0200

    maps: update

--- data/map/game.tsx
+++ data/map/game.tsx
@@ -8,10 +8,10 @@
   <image width="64" height="40" source="../image/wall.png"/>
  </tile>
  <tile id="2" type="solid">
-  <image width="8" height="8" source="../image/gate.png"/>
+  <image width="64" height="40" source="../image/gate.png"/>
  </tile>
  <tile id="3" type="shot">
-  <image width="8" height="8" source="../image/canon.png"/>
+  <image width="64" height="40" source="../image/canon.png"/>
  </tile>
  <tile id="4" type="death">
   <image width="64" height="40" source="../image/trap.png"/>
@@ -23,52 +23,52 @@
   <image width="64" height="40" source="../image/exit.png"/>
  </tile>
  <tile id="7">
-  <image width="8" height="8" source="../image/letter_a.png"/>
+  <image width="64" height="40" source="../image/letter_a.png"/>
  </tile>
  <tile id="8">
-  <image width="8" height="8" source="../image/letter_b.png"/>
+  <image width="64" height="40" source="../image/letter_b.png"/>
  </tile>
  <tile id="9">
-  <image width="8" height="8" source="../image/letter_c.png"/>
+  <image width="64" height="40" source="../image/letter_c.png"/>
  </tile>
  <tile id="10">
-  <image width="8" height="8" source="../image/letter_g.png"/>
+  <image width="64" height="40" source="../image/letter_g.png"/>
  </tile>
  <tile id="11">
-  <image width="8" height="8" source="../image/letter_h.png"/>
+  <image width="64" height="40" source="../image/letter_h.png"/>
  </tile>
  <tile id="12">
-  <image width="8" height="8" source="../image/letter_i.png"/>
+  <image width="64" height="40" source="../image/letter_i.png"/>
  </tile>
  <tile id="13">
-  <image width="8" height="8" source="../image/letter_k.png"/>
+  <image width="64" height="40" source="../image/letter_k.png"/>
  </tile>
  <tile id="14">
-  <image width="8" height="8" source="../image/letter_m.png"/>
+  <image width="64" height="40" source="../image/letter_m.png"/>
  </tile>
  <tile id="15">
-  <image width="8" height="8" source="../image/letter_n.png"/>
+  <image width="64" height="40" source="../image/letter_n.png"/>
  </tile>
  <tile id="16">
-  <image width="8" height="8" source="../image/letter_r.png"/>
+  <image width="64" height="40" source="../image/letter_r.png"/>
  </tile>
  <tile id="17">
-  <image width="8" height="8" source="../image/letter_t.png"/>
+  <image width="64" height="40" source="../image/letter_t.png"/>
  </tile>
  <tile id="18">
-  <image width="8" height="8" source="../image/letter_u.png"/>
+  <image width="64" height="40" source="../image/letter_u.png"/>
  </tile>
  <tile id="19">
-  <image width="8" height="8" source="../image/letter_y.png"/>
+  <image width="64" height="40" source="../image/letter_y.png"/>
  </tile>
  <tile id="20">
-  <image width="8" height="8" source="../image/digit_0.png"/>
+  <image width="64" height="40" source="../image/digit_0.png"/>
  </tile>
  <tile id="21">
-  <image width="8" height="8" source="../image/digit_1.png"/>
+  <image width="64" height="40" source="../image/digit_1.png"/>
  </tile>
  <tile id="22">
-  <image width="8" height="8" source="../image/digit_8.png"/>
+  <image width="64" height="40" source="../image/digit_8.png"/>
  </tile>
  <tile id="23">
   <image width="64" height="40" source="../image/trap_rev.png"/>
